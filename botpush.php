d29a5a11ae3154334097d17a3e3a3122b1ab8ff8
jareuk <31555484+jareuk@users.noreply.github.com>
2018-12-21 07:44:48 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,7 +4,7 @@
 
 require "vendor/autoload.php";
 
-$access_token = 's80Es7+OflmR5Rrbkvd2pEzQ+9QDEkApU5uC99E2Vg5s8fx31yO+6wO4PG7e0kpKD+8FAw/kj1XYU6qfZKp4PBcOPxYt98ruyx3OL5FAc+rUFa2uK05rX7yb45P+D6esbX5DGLwQVe6IyDwJR1L4rgdB04t89/1O/w1cDnyilFU=';
+$access_token = require "token.php";
 
 $channelSecret = '1ece6e120e4cc59ae01e8b684d3882c8';
 
