9351c67530967e0e4ced1460cb6618a6f29dfb89
jareuk <31555484+jareuk@users.noreply.github.com>
2018-12-21 07:42:21 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 's80Es7+OflmR5Rrbkvd2pEzQ+9QDEkApU5uC99E2Vg5s8fx31yO+6wO4PG7e0kpKD+8FAw/kj1XYU6qfZKp4PBcOPxYt98ruyx3OL5FAc+rUFa2uK05rX7yb45P+D6esbX5DGLwQVe6IyDwJR1L4rgdB04t89/1O/w1cDnyilFU=';
+$access_token = require "token.php";
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
